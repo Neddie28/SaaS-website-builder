2fbdf66f0d76241055c131a3aacfb0d9fdc40b8b
Neddie28 <Neddiecornel001@gmail.com>
2024-05-10 17:47:51 +0100

    a new script

--- src/app/(main)/agency/[agencyId]/all-subaccounts/_components/create-subaccount-btn.tsx
+++ src/app/(main)/agency/[agencyId]/all-subaccounts/_components/create-subaccount-btn.tsx
@@ -1,30 +1,40 @@
-'use client'
-import SubAccountDetails from '@/components/forms/subaccount-details'
-import CustomModal from '@/components/global/custom-modal'
-import { Button } from '@/components/ui/button'
-import { useModal } from '@/providers/modal-provider'
-import { Agency, AgencySidebarOption, SubAccount, User } from '@prisma/client'
-import { PlusCircleIcon } from 'lucide-react'
-import React from 'react'
-import { twMerge } from 'tailwind-merge'
+'use client';
+
+import {
+  Agency,
+  AgencySidebarOption,
+  SubAccount,
+  User
+} from '@prisma/client';
+
+import { PlusCircleIcon } from 'lucide-react';
+
+import { useModal } from '@/providers/modal-provider';
+
+import  SubAccountDetails  from '@/components/forms/subaccount-details';
+import CustomModal from '@/components/global/custom-modal';
+import { Button } from '@/components/ui/button';
+
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
   user: User & {
+    // The Agency property can be either an object of type Agency or a nullable object with additional properties SubAccount and SideBarOption.
     Agency:
-      | (
-          | Agency
-          | (null & {
-              SubAccount: SubAccount[]
-              SideBarOption: AgencySidebarOption[]
-            })
-        )
-      | null
+    | (
+      | Agency
+      | (null & {
+        SubAccount: SubAccount[]
+        SideBarOption: AgencySidebarOption[]
+      })
+    )
+    | null
   }
   id: string
   className: string
 }
 
-const CreateSubaccountButton = ({ className, id, user }: Props) => {
+export const CreateSubaccountButton = ({ className, id, user }: Props) => {
   const { setOpen } = useModal()
   const agencyDetails = user.Agency
 
@@ -37,7 +47,7 @@ const CreateSubaccountButton = ({ className, id, user }: Props) => {
         setOpen(
           <CustomModal
             title="Create a Subaccount"
-            subheading="You can switch bettween"
+            subheading="You can switch between"
           >
             <SubAccountDetails
               agencyDetails={agencyDetails}
@@ -53,5 +63,3 @@ const CreateSubaccountButton = ({ className, id, user }: Props) => {
     </Button>
   )
 }
-
-export default CreateSubaccountButton
