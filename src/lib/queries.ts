3004d996a6036c4b5d39526eb479886dd03726f1
Chinaka Chinedu <143916594+Neddie28@users.noreply.github.com>
2024-09-03 23:06:54 +0100

    Update queries.ts

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -170,7 +170,6 @@ export const verifyAndAcceptInvitation = async () => {
         subaccountId: undefined,
     })
 
-
     if(userDetails) {
         await clerkClient.users.updateUserMetadata(user.id, {
             privateMetadata: {
